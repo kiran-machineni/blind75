d13a2671453a3a2f03aa7a14b32cc41b9dc67ee0
Kiran Kumar Machineni <kiran-machineni@users.noreply.github.com>
2023-02-20 14:50:49 -0500

    twoSum

---
+++ src/TwoSum.js
@@ -0,0 +1,9 @@
+const twoSum = (nums, target) => {
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < nums.length; j++) {
+			if (i != j && nums[i] + nums[j] === target) return [i, j]
+		}
+	}
+}
+
+module.exports = { twoSum }
